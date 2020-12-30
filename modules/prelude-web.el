0528bb6a6e2ffce154ac331a7f6a3bfbf714a1bc
Geoffrey Mon <geofbot@gmail.com>
2020-12-30 15:51:59 -0500

    jsx using web-mode and don't rustfmt on save

--- modules/prelude-web.el
+++ modules/prelude-web.el
@@ -38,6 +38,7 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
