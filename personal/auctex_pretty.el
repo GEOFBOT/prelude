775ccc3894cb0dd24da2435c979038b1f308d9b7
Geoffrey Mon <geofbot@gmail.com>
2024-05-24 18:37:30 -0500

    tex outline mode

--- personal/auctex_pretty.el
+++ personal/auctex_pretty.el
@@ -1,4 +1,9 @@
 (add-hook 'TeX-mode-hook 'prettify-symbols-mode)
 
+(add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+
 (add-hook 'TeX-mode-hook (lambda ()
                            (text-scale-set 3)))
+
+;; (add-hook 'TeX-mode-hook (lambda ()
+;;                            (TeX-fold-mode 1)))
